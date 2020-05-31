3a59a6d90f8f6d484177c2ec3c026967f2e4795a
infarna <38402339+infarna@users.noreply.github.com>
2020-05-31 18:37:00 +0530

    Add files via upload

---
+++ basicproject/views.py
@@ -0,0 +1,28 @@
+from django.views import View
+from django.contrib.auth import get_user
+from genre.models import Genre, GenreFellow
+from posts.models import Post
+from django.shortcuts import render
+
+
+class HomePage(View):
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            association_list = []
+            post_to_displayed = []
+            user_logged_in = get_user(request)
+            print(user_logged_in.username)
+            for item in GenreFellow.objects.all():
+                print(item)
+                if item.user == user_logged_in:
+                    print(item.user)
+                    association_list.append(item.genre)
+            for post_item in Post.objects.all():
+                if post_item.genre in association_list:
+                    post_to_displayed.append(post_item)
+            return render(request, 'user_home.html',
+                          {'post_list': post_to_displayed})
+        else:
+            return render(request, 'index.html')
+
